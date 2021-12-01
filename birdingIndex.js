715152de55654d551ecf962b5561ee18690d3cb3
Graham <graham.lim88@gmail.com>
2021-12-01 22:47:26 +0800

    base on the way

---
+++ birdingIndex.js
@@ -0,0 +1,59 @@
+import express from 'express';
+import pg from 'pg';
+import methodOverride from 'method-override';
+
+// Initialise DB connection
+const { Pool } = pg;
+const pgConnectionConfigs = {
+  user: 'grahamlim',
+  host: 'localhost',
+  database: 'birdwatching',
+  port: 5432, // Postgres server always runs on this port by default
+};
+const pool = new Pool(pgConnectionConfigs);
+
+const app = express();
+
+const PORT = 3008;
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride('_method'));
+
+app.get('/note', (request, response) => {
+  console.log('note form get request came in');
+
+  const whenDoneWithQuery = (error, result) => {
+    if (error) {
+      console.log('Error executing query', error.stack);
+      response.status(503).send(result.rows);
+      return;
+    }
+    console.log(result.rows[0]);
+    response.render('noteForm');
+  };
+
+  // Query using pg.Pool instead of pg.Client
+  pool.query('SELECT * FROM birds', whenDoneWithQuery);
+});
+app.get('/', (request, response) => {
+  console.log('request came in');
+
+  const whenDoneWithQuery = (error, result) => {
+    if (error) {
+      console.log('Error executing query', error.stack);
+      response.status(503).send(result.rows);
+      return;
+    }
+    console.log(result.rows[0].name);
+    response.send(result.rows);
+  };
+
+  // Query using pg.Pool instead of pg.Client
+  pool.query('SELECT * FROM birds', whenDoneWithQuery);
+});
+
+app.listen(PORT);
+
+console.log(`listening on port ${PORT}`);
