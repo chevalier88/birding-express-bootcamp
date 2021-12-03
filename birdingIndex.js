651ed61ea16f3e1ea97d6879f143253947315dfe
Graham <graham.lim88@gmail.com>
2021-12-03 18:27:50 +0800

    3-pce-5 base done

--- birdingIndex.js
+++ birdingIndex.js
@@ -40,26 +40,43 @@ app.get('/note', (request, response) => {
 
 app.post('/note', (request, response) => {
   console.log('note form post request came in');
-
   const formData = request.body;
   console.log(formData);
 
-  const postBirdFormQuery = (error, result) => {
+  const { date } = formData;
+  const { behaviour } = formData;
+  const flockSize = formData.flock_size;
+  const { appearance } = formData;
+
+  const postBirdFormQuery = `
+  INSERT INTO birds (date, behaviour, flock_size, appearance)
+  VALUES ('${date}', '${behaviour}', ${flockSize}, '${appearance}');
+  `;
+  console.log(postBirdFormQuery);
+  const postBirdFormQueryResult = (error, result) => {
     if (error) {
       console.log('Error executing query', error.stack);
-      response.status(503).send(result.rows);
+      response.status(503).send(result);
       return;
     }
-    console.log(result.rows[0]);
-    response.send('birdwatching form new note submitted!');
+    console.log(result);
+
+    response.send('birdwatching form new note submitted! SQL Table has been updated...');
   };
 
   // Query using pg.Pool instead of pg.Client
-  pool.query('SELECT * FROM birds', postBirdFormQuery);
+  pool.query(postBirdFormQuery, postBirdFormQueryResult);
 });
 
-app.get('/', (request, response) => {
-  console.log('request came in');
+app.get('/note/:id', (request, response) => {
+  console.log('indiv note request came in');
+
+  console.log(request.params.id);
+
+  const getBirdNoteIndexQuery = `
+  SELECT * FROM birds WHERE id=${request.params.id};`;
+
+  console.log(getBirdNoteIndexQuery);
 
   const whenDoneWithQuery = (error, result) => {
     if (error) {
@@ -67,12 +84,47 @@ app.get('/', (request, response) => {
       response.status(503).send(result.rows);
       return;
     }
-    console.log(result.rows[0].name);
-    response.send(result.rows);
+    console.log(result.rows[0]);
+    const content = {
+      noteIndex: {
+        id: result.rows[0].id,
+        date: result.rows[0].date,
+        behaviour: result.rows[0].behaviour,
+        flock_size: result.rows[0].flock_size,
+        appearance: result.rows[0].appearance,
+      },
+    };
+    console.log(content);
+    // response.send(result.rows[0]);
+    response.render('noteIndex', content);
   };
 
   // Query using pg.Pool instead of pg.Client
-  pool.query('SELECT * FROM birds', whenDoneWithQuery);
+  pool.query(getBirdNoteIndexQuery, whenDoneWithQuery);
+});
+
+app.get('/', (request, response) => {
+  console.log('indiv note request came in');
+
+  const getAllBirdNotesQuery = `
+  SELECT * FROM birds;`;
+
+  const whenDoneWithQuery = (error, result) => {
+    if (error) {
+      console.log('Error executing query', error.stack);
+      response.status(503).send(result.rows);
+      return;
+    }
+    console.log(result.rows);
+    const content = {
+      allSightings: result.rows,
+    };
+    // response.send(content);
+    response.render('allNotes', content);
+  };
+
+  // Query using pg.Pool instead of pg.Client
+  pool.query(getAllBirdNotesQuery, whenDoneWithQuery);
 });
 
 app.listen(PORT);
